5099ae3f4c1d595ff4c6d43942851fc7d26f432b
mnaibei <muchajulius@gmail.com>
2021-06-17 02:14:17 -0700

    ,

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+ int main()
+ {
+     puts("Programming is like building a multilingual puzzle\n");
+     return 0;
+ }
