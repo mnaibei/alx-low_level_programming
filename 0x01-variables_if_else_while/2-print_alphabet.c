416dc4283524a4fac11e79ef1c74e56aa9aa55ca
mnaibei <muchajulius@gmail.com>
2021-06-18 06:03:46 -0700

    .

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,9 @@
  */
 int main(void)
 {
-
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
+	char x;
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
 		putchar('\n');
-
-return (0);
+		return (0);
 }
