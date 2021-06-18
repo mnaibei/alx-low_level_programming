96a19331a89005c436f6919940b0b08c86da81d2
mnaibei <muchajulius@gmail.com>
2021-06-18 05:59:44 -0700

    .

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 /**
- *main - Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
+
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
