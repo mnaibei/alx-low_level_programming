e7f2b16ae667368125f8c89f9dc6ce09a5027a8d
mnaibei <muchajulius@gmail.com>
2021-06-24 03:38:37 -0700

    .

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,29 @@
 #include "holberton.h"
-
 /**
- * main - print sign of a number.
- *
- * Return: Always 0.
- */
+* print_sign - Determines if the input number
+* greater, iqual or less than zero.
+*
+* @n: The input number as an integer.
+*
+* Return: 1 is greater than zero. 0 is zero.
+* -1 is less than zero.
+*/
 int print_sign(int n)
 {
 	if (n > 0)
-	{
-		return (1);
+		{
 		_putchar(43);
-	}
-	else if (n == 0)
-	{
-		return (0);
+	return (1);
+		}
+	else if (n < 0)
+		{
 		_putchar(45);
-	}
+	return (-1);
+		}
 	else
-	{
-		return (-1);
+		{
 		_putchar(48);
-	}
-	_putchar('\n');
+	return (0);
+		}
+		_putchar('\n');
 }
