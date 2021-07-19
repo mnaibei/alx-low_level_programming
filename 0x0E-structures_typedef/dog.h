821a4f70a9b9af1c71c15d05ac00c4c1dfc56efe
mnaibei <muchajulius@gmail.com>
2021-07-19 08:06:10 -0700

    typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE dog.h
+/**
+ *struct dog - defines dog xtics
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of the dog
+ **/
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+#endif
